eab04b9b81bffe6a887ca5a4d4982afad6f31701
AdriannaKoz <a.a.kozimor@gmail.com>
2026-02-21 13:56:16 +0000

    Add button click

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("https://www.wp.pl/")
+driver.get("https://automationpractice.techwithjatin.com/")
 driver.maximize_window()
+sign_in_a = driver.find_element(By.XPATH, "//a[@title='Log in to your customer account']")
+sign_in_a.click()
 sleep(5)
 driver.quit()
